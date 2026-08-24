90e97c8d19ac586246c4a658f836b4041a850ef2
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2026-08-24 20:43:19 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -8,7 +8,7 @@
 const API_CONFIG = {
     // THAY THẾ ĐƯỜNG LINK DƯỚI ĐÂY BẰNG URL WEB APP CỦA BẠN (Lấy từ Google Apps Script sau khi Deploy)
     // Ví dụ: "https://script.google.com/macros/s/AKfycb.../exec"
-    BASE_URL: "https://script.google.com/macros/s/AKfycbwj9U_4bDiVWbU1489q_wh8y06KlNKaXPjK46uMg7itUQ6NdCA_cAixtiz_WGfzm0lf/exec" 
+    BASE_URL: "https://script.google.com/macros/s/AKfycbzM3eI-tzBahTusZdpAakz94Sz_ttiWt6Gmf2Y1dFt-SWrZ_dMek_CIo7njffNM_Y0L/exec" 
 };
 
 const SlideAPI = {
